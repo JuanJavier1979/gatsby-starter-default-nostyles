e9dea16b8acbfa5b280c8e3790d97681de6e8b36
JuanJavier1979 <jjmrestituto@gmail.com>
2020-01-07 21:19:02 +0100

    Remove styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Default Starter with no Styles`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need and no styles.`,
+    author: `@JuanJavier1979`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
